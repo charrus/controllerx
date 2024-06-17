7aa65f60dfdb2927da94f7d0e072cf7afabb1981
charrus <charlie@rusbridger.com>
2024-06-17 19:03:34 +0100

    Add Z2M for Osram AC0251100NJ

--- apps/controllerx/cx_devices/osram.py
+++ apps/controllerx/cx_devices/osram.py
@@ -11,22 +11,31 @@ class OsramAC025XX00NZ2MJLightController(Z2MLightController):
         return {
             # Click Arrow up
             "on": Z2MLight.ON,
+            "up": Z2MLight.ON,
             # Hold Arrow Up
             "brightness_move_up": Z2MLight.HOLD_BRIGHTNESS_UP,
+            "up_hold": Z2MLight.HOLD_BRIGHTNESS_UP,
             # Release Arrow Up
             "brightness_stop": Z2MLight.RELEASE,
+            "up_release": Z2MLight.RELEASE,
             # Click Circle button
             "brightness_move_to_level": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             "color_temperature_move": Z2MLight.COLORTEMP_FROM_CONTROLLER,
+            "circle_click": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             # Hold Circle button
             "move_to_saturation": None,
             "hue_move": Z2MLight.HOLD_COLOR_TEMP_UP,
+            "circle_hold": Z2MLight.HOLD_COLOR_TEMP_UP,
             # Release Circle button
             "hue_stop": Z2MLight.RELEASE,
             # Click Arrow down
             "off": Z2MLight.OFF,
+            "down": Z2MLight.OFF,
             # Hold Arrow down
             "brightness_move_down": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            "down_hold": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            # Release Arrow Down
+            "down_release": Z2MLight.RELEASE,
         }
 
 
@@ -38,22 +47,32 @@ class OsramAC025XX00NJLightController(LightController):
         return {
             # Click Arrow up
             "on": Light.ON,
+            "up": Light.ON,
             # Hold Arrow Up
             "brightness_move_up": Light.HOLD_BRIGHTNESS_UP,
+            "up_hold": Light.HOLD_BRIGHTNESS_UP,
             # Release Arrow Up
             "brightness_stop": Light.RELEASE,
+            "up_release": Light.RELEASE,
             # Click Circle button
             "brightness_move_to_level": Light.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             "color_temperature_move": Light.COLORTEMP_FROM_CONTROLLER,
+            "circle_click": Light.SYNC,
             # Hold Circle button
             "move_to_saturation": None,
-            "hue_move": Light.HOLD_COLOR_TEMP_UP,
+            "hue_move": Light.HOLD_COLOR_UP,
+            "circle_hold": Light.HOLD_COLOR_UP,
             # Release Circle button
             "hue_stop": Light.RELEASE,
+            "circle_release": Light.RELEASE,
             # Click Arrow down
             "off": Light.OFF,
+            "down": Light.OFF,
             # Hold Arrow down
             "brightness_move_down": Light.HOLD_BRIGHTNESS_DOWN,
+            "down_hold": Light.HOLD_BRIGHTNESS_DOWN,
+            # Release Arrow Down
+            "down_release": Light.RELEASE,
         }
 
     def get_deconz_actions_mapping(self) -> DefaultActionsMapping:
