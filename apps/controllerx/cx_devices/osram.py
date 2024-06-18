c59c08bf398199ac842b1d6012ff5c34bba17f23
charrus <charlie@rusbridger.com>
2024-06-18 15:58:54 +0100

    Tydy up z2m osram

--- apps/controllerx/cx_devices/osram.py
+++ apps/controllerx/cx_devices/osram.py
@@ -11,30 +11,39 @@ class OsramAC025XX00NZ2MJLightController(Z2MLightController):
         return {
             # Click Arrow up
             "on": Z2MLight.ON,
+            # Click Arrow up (legacy)
             "up": Z2MLight.ON,
             # Hold Arrow Up
             "brightness_move_up": Z2MLight.HOLD_BRIGHTNESS_UP,
+            # Hold Arrow Up (legacy)
             "up_hold": Z2MLight.HOLD_BRIGHTNESS_UP,
             # Release Arrow Up
             "brightness_stop": Z2MLight.RELEASE,
+            # Release Arrow Up (legacy)
             "up_release": Z2MLight.RELEASE,
             # Click Circle button
             "brightness_move_to_level": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             "color_temperature_move": Z2MLight.COLORTEMP_FROM_CONTROLLER,
+            # Click Circle button (legacy)
             "circle_click": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             # Hold Circle button
             "move_to_saturation": None,
             "hue_move": Z2MLight.HOLD_COLOR_TEMP_UP,
+            # Hold Circle button (legacy)
             "circle_hold": Z2MLight.HOLD_COLOR_TEMP_UP,
             # Release Circle button
             "hue_stop": Z2MLight.RELEASE,
             # Click Arrow down
             "off": Z2MLight.OFF,
+            # Click Arrow down (legacy)
             "down": Z2MLight.OFF,
             # Hold Arrow down
             "brightness_move_down": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            # Hold Arrow down (legacy)
             "down_hold": Z2MLight.HOLD_BRIGHTNESS_DOWN,
-            # Release Arrow Down
+            # Release Arrow Down (Taken care of by up release)
+            # "brightness_stop": Z2MLight.RELEASE,
+            # Release Arrow Down (legacy)
             "down_release": Z2MLight.RELEASE,
         }
 
@@ -47,31 +56,41 @@ class OsramAC025XX00NJLightController(LightController):
         return {
             # Click Arrow up
             "on": Light.ON,
+            # Click Arrow up (legacy)
             "up": Light.ON,
             # Hold Arrow Up
             "brightness_move_up": Light.HOLD_BRIGHTNESS_UP,
+            # Hold Arrow Up (legacy)
             "up_hold": Light.HOLD_BRIGHTNESS_UP,
             # Release Arrow Up
             "brightness_stop": Light.RELEASE,
+            # Release Arrow Up (legacy)
             "up_release": Light.RELEASE,
             # Click Circle button
             "brightness_move_to_level": Light.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
             "color_temperature_move": Light.COLORTEMP_FROM_CONTROLLER,
+            # Click Circle button (legacy)
             "circle_click": Light.SYNC,
             # Hold Circle button
             "move_to_saturation": None,
             "hue_move": Light.HOLD_COLOR_UP,
+            # Hold Circle button (legacy)
             "circle_hold": Light.HOLD_COLOR_UP,
             # Release Circle button
             "hue_stop": Light.RELEASE,
+            # Release Circle button (legacy)
             "circle_release": Light.RELEASE,
             # Click Arrow down
             "off": Light.OFF,
+            # Click Arrow down (legacy)
             "down": Light.OFF,
             # Hold Arrow down
             "brightness_move_down": Light.HOLD_BRIGHTNESS_DOWN,
+            # Hold Arrow down (legacy)
             "down_hold": Light.HOLD_BRIGHTNESS_DOWN,
-            # Release Arrow Down
+            # Release Arrow Down (Taken care of by up release)
+            # "brightness_stop": Light.RELEASE,
+            # Release Arrow Down (legacy)
             "down_release": Light.RELEASE,
         }
 
