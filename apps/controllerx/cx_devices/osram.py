1c0d8a6ed2ac45c15d2e2ecb4db224ccbacf5cf8
charrus <charlie@rusbridger.com>
2024-06-18 16:46:51 +0100

    Tydy up z2m osram

--- apps/controllerx/cx_devices/osram.py
+++ apps/controllerx/cx_devices/osram.py
@@ -3,54 +3,10 @@ from cx_core import LightController, Z2MLightController
 from cx_core.integration import EventData
 
 
-class OsramAC025XX00NZ2MJLightController(Z2MLightController):
-    # This mapping works for: AC0251100NJ / AC0251400NJ / AC0251600NJ / AC0251700NJ
-    # (different models are just different colours)
-
-    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
-        return {
-            # Click Arrow up
-            "on": Z2MLight.ON,
-            # Click Arrow up (legacy)
-            "up": Z2MLight.ON,
-            # Hold Arrow Up
-            "brightness_move_up": Z2MLight.HOLD_BRIGHTNESS_UP,
-            # Hold Arrow Up (legacy)
-            "up_hold": Z2MLight.HOLD_BRIGHTNESS_UP,
-            # Release Arrow Up
-            "brightness_stop": Z2MLight.RELEASE,
-            # Release Arrow Up (legacy)
-            "up_release": Z2MLight.RELEASE,
-            # Click Circle button
-            "brightness_move_to_level": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
-            "color_temperature_move": Z2MLight.COLORTEMP_FROM_CONTROLLER,
-            # Click Circle button (legacy)
-            "circle_click": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
-            # Hold Circle button
-            "move_to_saturation": None,
-            "hue_move": Z2MLight.HOLD_COLOR_TEMP_UP,
-            # Hold Circle button (legacy)
-            "circle_hold": Z2MLight.HOLD_COLOR_TEMP_UP,
-            # Release Circle button
-            "hue_stop": Z2MLight.RELEASE,
-            # Click Arrow down
-            "off": Z2MLight.OFF,
-            # Click Arrow down (legacy)
-            "down": Z2MLight.OFF,
-            # Hold Arrow down
-            "brightness_move_down": Z2MLight.HOLD_BRIGHTNESS_DOWN,
-            # Hold Arrow down (legacy)
-            "down_hold": Z2MLight.HOLD_BRIGHTNESS_DOWN,
-            # Release Arrow Down (Taken care of by up release)
-            # "brightness_stop": Z2MLight.RELEASE,
-            # Release Arrow Down (legacy)
-            "down_release": Z2MLight.RELEASE,
-        }
-
-
 class OsramAC025XX00NJLightController(LightController):
     # This mapping works for: AC0251100NJ / AC0251400NJ / AC0251600NJ / AC0251700NJ
     # (different models are just different colours)
+    # Zigbee2MQTT works both with the device setting legacy on or off.
 
     def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
         return {
@@ -88,7 +44,7 @@ class OsramAC025XX00NJLightController(LightController):
             "brightness_move_down": Light.HOLD_BRIGHTNESS_DOWN,
             # Hold Arrow down (legacy)
             "down_hold": Light.HOLD_BRIGHTNESS_DOWN,
-            # Release Arrow Down (Taken care of by up release)
+            # Release Arrow Down (Taken care of by Release Arrow Up)
             # "brightness_stop": Light.RELEASE,
             # Release Arrow Down (legacy)
             "down_release": Light.RELEASE,
@@ -123,3 +79,49 @@ class OsramAC025XX00NJLightController(LightController):
 
     def get_zha_action(self, data: EventData) -> str:
         return f"{data['endpoint_id']}_{data['command']}"
+
+
+class OsramAC025XX00NJZ2MLightController(Z2MLightController):
+    # This mapping works for: AC0251100NJ / AC0251400NJ / AC0251600NJ / AC0251700NJ
+    # (different models are just different colours)
+    # Zigbee2MQTT works both with the device setting legacy on or off.
+
+    def get_z2m_actions_mapping(self) -> DefaultActionsMapping:
+        return {
+            # Click Arrow up
+            "on": Z2MLight.ON,
+            # Click Arrow up (legacy)
+            "up": Z2MLight.ON,
+            # Hold Arrow Up
+            "brightness_move_up": Z2MLight.HOLD_BRIGHTNESS_UP,
+            # Hold Arrow Up (legacy)
+            "up_hold": Z2MLight.HOLD_BRIGHTNESS_UP,
+            # Release Arrow Up
+            "brightness_stop": Z2MLight.RELEASE,
+            # Release Arrow Up (legacy)
+            "up_release": Z2MLight.RELEASE,
+            # Click Circle button
+            "brightness_move_to_level": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
+            "color_temperature_move": Z2MLight.COLORTEMP_FROM_CONTROLLER,
+            # Click Circle button (legacy)
+            "circle_click": Z2MLight.BRIGHTNESS_FROM_CONTROLLER_LEVEL,
+            # Hold Circle button
+            "move_to_saturation": None,
+            "hue_move": Z2MLight.HOLD_COLOR_TEMP_UP,
+            # Hold Circle button (legacy)
+            "circle_hold": Z2MLight.HOLD_COLOR_TEMP_UP,
+            # Release Circle button
+            "hue_stop": Z2MLight.RELEASE,
+            # Click Arrow down
+            "off": Z2MLight.OFF,
+            # Click Arrow down (legacy)
+            "down": Z2MLight.OFF,
+            # Hold Arrow down
+            "brightness_move_down": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            # Hold Arrow down (legacy)
+            "down_hold": Z2MLight.HOLD_BRIGHTNESS_DOWN,
+            # Release Arrow Down (Taken care of by Release Arrow Up)
+            # "brightness_stop": Z2MLight.RELEASE,
+            # Release Arrow Down (legacy)
+            "down_release": Z2MLight.RELEASE,
+        }
